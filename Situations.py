b3518aae98f71226b630f6962857de66d0adfeab
Alan Li <chemist753@gmail.com>
2023-04-01 11:39:42 -0400

    created Situations base template and means to generate a Situation

--- Situations.py
+++ Situations.py
@@ -1,7 +1,27 @@
-import variables
-class Situations:
+descriptions = {
+        0: "insert desc here",
+        1: "insert desc here"
+    }
 
-    def __init__(self, vars):
-        self.money = vars.money
-        self.happiness = vars.happiness
-        self.carbon = vars.carbon
+deltaMoney = 1
+deltaCO2 = 1
+deltaHappiness = 1
+
+outcome1s = {
+    0: ("desc here", deltaMoney, deltaCO2, deltaHappiness)
+}
+
+outcome2s = {
+    0: ("desc here", deltaMoney, deltaCO2, deltaHappiness)
+}
+
+outcome3s = {
+    0: ("desc here", deltaMoney, deltaCO2, deltaHappiness)
+}
+
+def createSituation(num):
+    print(descriptions[num])
+    print("What will you do?")
+    print("1 - " + outcome1s[num])
+    print("2 - " + outcome2s[num])
+    print("3 - " + outcome3s[num])
