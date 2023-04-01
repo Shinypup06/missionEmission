9bcd80e0092eb9e91a88d3b21629efd821f772de
Alan Li <chemist753@gmail.com>
2023-04-01 14:09:57 -0400

    updated situation mechanism

--- Situations.py
+++ Situations.py
@@ -20,8 +20,20 @@ outcome3s = {
 }
 
 def createSituation(num):
+    hasOutcome3 = num in outcome3s.keys()
     print(descriptions[num])
-    print("What will you do?")
-    print("1 - " + outcome1s[num])
-    print("2 - " + outcome2s[num])
-    print("3 - " + outcome3s[num])
+    while True:
+        print("What will you do?")
+        print("1 - " + outcome1s[num][0])
+        print("2 - " + outcome2s[num][0])
+        if (hasOutcome3):
+            print("3 - " + outcome3s[num][0])
+        choice = input()
+        if int(choice) == 1:
+            return outcome1s[num][1:3]
+        if int(choice) == 2:
+            return outcome2s[num][1:3]
+        if hasOutcome3 and int(choice) == 3:
+            return outcome3s[num][1:3]
+        
+
