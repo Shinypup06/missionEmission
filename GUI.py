8e16da40768bec636b03eb1407e633b2832357e6
Shining <shinypup06@gmail.com>
2023-04-01 10:26:49 -0400

    gui update

--- GUI.py
+++ GUI.py
@@ -4,6 +4,9 @@ import tkinter as tk
 HEIGHT = 700
 WIDTH = 1200
 
+def closeTutorial():
+    tutorialFrame.lower()
+    mainFrame.lift()
 
 root = tk.Tk()
 
@@ -11,11 +14,41 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-gameFrame = tk.Frame(root, bg="#f4f1e4")
-gameFrame.place(relheight=1, relwidth=0.8, relx = 0.2, rely=0)
 
-actionBar = tk.Frame(root, bg="#efe9d4")
-actionBar.place(relheight=1, relwidth=0.2, relx=0, rely=0)
 
-root.title("Carbon Game")
+mainFrame = tk.Frame(root, bg="white")
+mainFrame.place(relheight = 1, relwidth=1, relx=0, rely=0)
+
+statsFrame = tk.Frame(mainFrame, bg="#f8f6f2")
+statsFrame.place(relheight=0.45, relwidth=0.7, relx = 0.3, rely=0.55)
+
+
+
+actionBar = tk.Frame(mainFrame, bg="#d6cfb7")
+actionBar.place(relheight=1, relwidth=0.3, relx=0, rely=0)
+
+actionBarTitle = tk.Label(actionBar, text="Your Actions", font=("Cambria", 15), bg="#f3efe1")
+actionBarTitle.place(relx = 0.05, rely = 0.05, relheight=0.06, relwidth=0.9)
+
+
+situationFrame = tk.Frame(actionBar, bg="#f3efe1")
+situationFrame.place(relwidth= 0.9, relheight= 0.5, relx = 0.05, rely = 0.15)
+
+
+tutorialFrame = tk.Frame(root, bg="white", bd=2, highlightbackground="#6b644e", highlightthickness=2)
+tutorialFrame.place(relx = 0.2, rely = 0.2, relheight=0.6, relwidth=0.6)
+
+tutorialTitle = tk.Label(tutorialFrame, font = ("Cambria", 16, "bold"), text= "Welcome to the Mission: Emission!", bg="white")
+tutorialTitle.place(relx = 0.1, rely=0.15, relheight=0.1, relwidth=0.8)
+
+tutorialText = tk.Label(tutorialFrame, font= ("Cambria", 12), text="Welcome to ")
+tutorialText.place(relx=0.05, rely=0.3, relheight=0.45, relwidth=0.8)
+
+
+tutorialOK=tk.Button(tutorialFrame, text= "Ok, let's play!", background="#f3efe1", font=("Cambria",16), activebackground="#fdfaf1", command=closeTutorial)
+tutorialOK.place(relx=0.3, rely=0.8, relheight=0.15, relwidth=0.4)
+
+
+
+root.title("Mission: Emission")
 root.mainloop()
